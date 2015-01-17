eae255a3d2b615be2687db19e36a08d49829320f
Malyshev Artem <proofit404@gmail.com>
2015-01-17 20:26:15 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+readme = open('README.rst').read()
+
+setup(name='service_factory',
+      version='0.1.0',
+      url='https://github.com/proofit404/service-factory',
+      description='JSON RPC service factory for Python.',
+      long_description=readme,
+      platforms='any',
+      license='GPL3',
+      author='Artem Malyshev',
+      author_email='proofit404@gmail.com',
+      maintainer='Artem Malyshev',
+      maintainer_email='proofit404@gmail.com',
+      packages=find_packages(exclude=['tests*']),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+      ])
