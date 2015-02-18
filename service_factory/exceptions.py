dea808490c70af64bc6057cec1d33e4a6a6d2239
Artem Malyshev <proofit404@gmail.com>
2015-02-19 00:17:13 +0300

    Import missing __future__.

--- service_factory/exceptions.py
+++ service_factory/exceptions.py
@@ -8,6 +8,9 @@
     :license: GPL3, see LICENSE for more details.
 """
 
+from __future__ import (
+    absolute_import, unicode_literals, division, print_function)
+
 
 class ServiceException(Exception):
     """Exception occurred in running service."""
